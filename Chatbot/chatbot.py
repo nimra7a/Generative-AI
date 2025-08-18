c04d15405d2108ef5a836d3721c5acee8e513ca0
nimra7a <168336139+nimra7a@users.noreply.github.com>
2025-08-18 13:41:15 +0500

    introducing LangChain Message Types

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -1,4 +1,5 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 import os
 import secrets
 
@@ -10,15 +11,16 @@ llm = HuggingFaceEndpoint(
 )
 
 model = ChatHuggingFace(llm=llm)
-chat_history = []
+
+chat_history = [SystemMessage(content="You're a helpful Assitant")]
 
 while True:
   user_input = input("user:")
   if user_input == "exit":
     break
-  chat_history.append(user_input)
+  chat_history.append(HumanMessage(content=user_input))
   response = model.invoke(chat_history)
-  chat_history.append(response.content)
+  chat_history.append(AIMessage(content=response.content))
   print("AI: ", response.content)
 
 print(chat_history)
