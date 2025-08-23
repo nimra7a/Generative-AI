feffb996759169fb76a41b880c75bb3338d57720
nimra7a <168336139+nimra7a@users.noreply.github.com>
2025-08-23 18:30:43 +0500

    Adding the descriptive report on Structured Output

--- Structured Output/with_structured_output_typeddict.py
+++ Structured Output/with_structured_output_typeddict.py
@@ -18,7 +18,9 @@ class Review(TypedDict):
 
 structured_model = model.with_structured_output(Review)
 
-result = structured_model.invoke("""Thardware is great, but the software feels bloated. There are too many pre-installed apps that I can't remove. Also, the UI looks outdated compared to other brands. Hoping for a software update to fix this.""")
+result = structured_model.invoke("""Thardware is great, but the software feels bloated. 
+There are too many pre-installed apps that I can't remove. Also, the UI looks outdated compared to other brands. 
+Hoping for a software update to fix this.""")
 
 print(result)
 print("Summary:", result['summary'])
