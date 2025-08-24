c7e3b739b495efb7f20d8539ff63a56e445e9e6b
nimra7a <168336139+nimra7a@users.noreply.github.com>
2025-08-24 12:57:57 +0500

    Understanding the concept of JsonOutputParser

---
+++ Output_Parsers/jsonoutputparser.py
@@ -0,0 +1,37 @@
+from ast import mod
+from langchain_core.output_parsers.json import JsonOutputParser
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+import os
+import secrets
+
+parser = JsonOutputParser()
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = secrets.HuggingFaceHub_ACCESS_TOKEN
+
+llm = HuggingFaceEndpoint(
+  repo_id="openai/gpt-oss-120b",
+  task = "text-generation"
+)
+model = ChatHuggingFace(llm=llm)
+
+template = PromptTemplate(
+    template = 'Give me the name, age and city of a fictional person \n {format_instruction}',
+    input_variables=[],
+    partial_variables={'format_instruction' : parser.get_format_instructions()}
+)
+
+#traditional way
+# prompt = template.format()
+# result = model.invoke(prompt)
+# final_result = parser.parse(result.content)
+
+#chaining
+chain = template | model | parser 
+result = chain.invoke({})
+print(result)
+
+
+
+
+
